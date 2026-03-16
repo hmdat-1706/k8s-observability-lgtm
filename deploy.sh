4608670bdb3a2de534eaf6d13a1ad20690ddde21
hmdat-1706 <hmdat1706@gmail.com>
2026-03-16 15:22:00 +0700

    security: remove hardcoded telegram secrets

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
-echo "Starting..."
 
-# 1. Tạo namespace
+set -e
+echo "Starting..."
+echo "Create namespaces..."
 kubectl create namespace bookinfo --dry-run=client -o yaml | kubectl apply -f -
 kubectl create namespace lgtm --dry-run=client -o yaml | kubectl apply -f -
 
 echo "Deploy Bookinfo & HPA service..."
-kubectl apply -f bookinfo.yaml -n bookinfo
-kubectl apply -f bookinfo-hpa.yaml
+kubectl apply -f app/bookinfo.yaml -n bookinfo
+kubectl set resources deployment productpage-v1 -n bookinfo \
+  --requests=cpu=100m,memory=128Mi \
+  --limits=cpu=500m,memory=512Mi
+kubectl apply -f app/bookinfo-hpa.yaml
 
-echo "Deploy LGTM stack..."
-# Cài Loki, Tempo, Mimir (dùng cấu hình mặc định hoặc file values nếu bạn có)
-helm upgrade --install loki grafana/loki -n lgtm -f loki-values.yaml
+echo "Update Helm repos..."
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
+helm repo update
+
+echo "Deploy LGTM stackk..."
+echo "Loki..."
+helm upgrade --install loki grafana/loki -n lgtm -f monitoring/loki-values.yaml
+
+echo "Tempo..."
 helm upgrade --install tempo grafana/tempo -n lgtm
+
+echo "Mimir..."
 helm upgrade --install mimir grafana/mimir-distributed -n lgtm
 
-# Cài OTel, Prometheus và Grafana (dùng các file values tụi mình đã config)
-helm upgrade --install otel-collector open-telemetry/opentelemetry-collector -n lgtm -f otel-values.yaml
-helm upgrade --install prometheus prometheus-community/prometheus -n lgtm -f prometheus-values.yaml
-helm upgrade --install grafana grafana/grafana -n lgtm --set service.type=NodePort --set service.nodePort=32000 -f grafana-values.yaml
+echo "OpenTelemetry Collector..."
+helm upgrade --install otel-collector open-telemetry/opentelemetry-collector -n lgtm -f monitoring/otel-values.yaml
 
-echo "Done!"
+echo "Prometheus..."
+helm upgrade --install prometheus prometheus-community/prometheus -n lgtm -f monitoring/prometheus-values.yaml
+
+echo "Grafana..."
+helm upgrade --install grafana grafana/grafana -n lgtm --set service.type=NodePort --set service.nodePort=32000 -f monitoring/grafana-values.yaml
+echo "Done!"s
